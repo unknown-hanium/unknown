00b7810f1cf18ce67cf60cd787f156344104e488
Titane22 <titane308@gmail.com>
2017-08-24 20:47:54 +0900

    Adding Boss Pattern

---
+++ Assets/Scripts/Enemy/Boss/Boss.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    enum Pattern { None,Hail,MeteorFall,Earthquake}
+    // entry time for patternChange() method
+    public float waitForPattern;
+    public float idleForShoot;
+    // when pattern was end, wait time for next pattern 
+    public float waitForNextPattern;
+    public float termForHail;
+    public GameObject bolt;
+    public GameObject meteor;
+    public GameObject bigMeteor;
+    public GameObject earthquake;
+    public int meteorCount;
+
+    private GameObject target;
+    private Animator anim;
+    private Pattern pattern;
+    private bool alive;// TODO after apply health system, if boss have not hp then alive convert to false
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        pattern = Pattern.None;
+        alive = true;
+        target= GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(Attack());
+	}
+	
+	IEnumerator Attack()
+    {
+        while (alive)
+        {
+            int shootCount = Random.Range(10, 15);
+            StartCoroutine(shoot(shootCount));
+            yield return new WaitForSeconds(waitForPattern);
+            patternChange();
+            switch (pattern)
+            {
+                case Pattern.Hail:
+                    for (int i = 0; i < meteorCount; i++)
+                    {
+                        hail();
+                        yield return new WaitForSeconds(termForHail);
+                    }
+                    break;
+                case Pattern.MeteorFall:
+                    meteorFall();
+                    break;
+                case Pattern.Earthquake:
+                    Earthquake();
+                    break;
+            }
+            yield return new WaitForSeconds(waitForNextPattern);
+        }
+    } 
+
+    void patternChange()
+    {
+        pattern = (Pattern)Random.Range(1, 4);
+    }
+
+    IEnumerator shoot(int count)
+    {
+        anim.SetBool("isTriggeredHail", false);
+        anim.SetBool("isTriggeredFallMeteor", false);
+        anim.SetBool("isTriggeredEarthquake", false);
+        for (int i = 0; i < count; i++)
+        {
+            yield return new WaitForSeconds(idleForShoot);
+            // calculate direction to target
+            Vector3 direction = target.transform.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            // Call Animation Energy Bolt to player
+            GameObject bullet = Instantiate(bolt);
+            bullet.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            bullet.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+        }
+    }
+    void hail()
+    {
+        // get Random position for rain
+        Vector3 RandomPos = new Vector3(Random.Range(-41, 31), Random.Range(-17, 42), 0);
+        // Call Hail and repeat 20 per 0.1f to random position
+        GameObject hail = Instantiate(meteor);
+        hail.transform.position = RandomPos;
+        //Debug.Log("is Hailing");
+        anim.SetBool("isTriggeredHail", true);
+        // TODO add a falling hail animation
+            // after animation apply collision
+    }
+
+    void meteorFall()
+    {
+        GameObject BigMeteor = Instantiate(bigMeteor);
+        BigMeteor.transform.position = target.transform.position;
+        anim.SetBool("isTriggeredFallMeteor", true);
+        // TODO add a falling meteor animation
+            // after animation apply collision
+    }
+
+    void Earthquake()
+    {
+        GameObject earth = Instantiate(earthquake);
+        earth.transform.position = new Vector2(transform.position.x, transform.position.y - 3.0f);
+        anim.SetBool("isTriggeredEarthquake", true);
+        // TODO add a split ground animation
+            // after animation apply collision
+    }
+
+    
+}
---
+++ Assets/Scripts/Enemy/Boss/bulletMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMover : MonoBehaviour {
+    Transform target;
+
+    public float speed;
+    public float dest;
+    // Use this for initialization
+    void Start () {
+        target = GetComponent<Transform>();
+        target.Translate(0, 2, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target.Translate(0, speed, 0);
+
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Called");
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
