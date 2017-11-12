6f8c2585474e2427aba37f9f2c642608dfe3d392
hajeongyeon <hjy---@hanmail.net>
2017-11-12 11:58:52 +0900

    remove a directory

--- unknown-master/Assets/Scripts/Explosion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Explosion : MonoBehaviour {
-	void Start () {
-		Destroy (this.gameObject, 0.4f);
-	}
-}
